2ab05bb9b544cdb2158ddd949a4cea9307ad1711
Asep Hendar <4sep.hendar@gmail.com>
2024-06-14 20:38:25 +0700

    install bahan nya dulu bre

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/package-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f0c9db1f24b655c08f05078dd7a326a6ffff2bad',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/package-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f0c9db1f24b655c08f05078dd7a326a6ffff2bad',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
